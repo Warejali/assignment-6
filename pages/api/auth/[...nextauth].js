b3aa8438b26e350822b273fecc7101a206113979
Warejali <waraj.com@gmail.com>
2023-10-28 14:11:15 +0600

    first commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,11 +12,13 @@ export default NextAuth({
     async jwt({ token, user }) {
       if (user?._id) token._id = user._id;
       if (user?.isAdmin) token.isAdmin = user.isAdmin;
+      if (user?.isVendor) token.isVendor = user.isVendor;
       return token;
     },
     async session({ session, token }) {
       if (token?._id) session.user._id = token._id;
       if (token?.isAdmin) session.user.isAdmin = token.isAdmin;
+      if (token?.isVendor) session.user.isVendor = token.isVendor;
       return session;
     },
   },
@@ -27,18 +29,22 @@ export default NextAuth({
         const user = await User.findOne({
           email: credentials.email,
         });
+
         await db.disconnect();
         if (user && bcryptjs.compareSync(credentials.password, user.password)) {
           return {
             _id: user._id,
             name: user.name,
             email: user.email,
-            image: 'f',
+            file: user.file,
             isAdmin: user.isAdmin,
+            isVendor: user.isVendor,
+            bidNumber: user.bidNumber,
           };
         }
         throw new Error('Invalid email or password');
       },
     }),
   ],
+  secret: process.env.API_SECRET_KEY,
 });
